d3bf1b890d5bf3c8977a06256c9a7b96f6d4c97a
Tomasz Posiadala <tomaszposiadala@gmail.com>
2020-09-03 00:37:02 +0100

    setting up webpack and my dev environment

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: { minimize: true }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  'style-loader',
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                  'sass-loader',
+                ],
+            },
+        ]
+    }, 
+    plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ]
+    };
