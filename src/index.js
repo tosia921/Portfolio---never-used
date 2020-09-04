8de62b452075727e28553f1bfcc098887375e9f2
Tomasz Posiadala <tomaszposiadala@gmail.com>
2020-09-04 23:30:22 +0100

    added animated humburger button, made LP responsive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles/main.scss';
 
-console.log('ssai');
+//Function to select elements
+
+const selectElement = (element) => document.querySelector(element);
+
+
+selectElement('.menu__btn').addEventListener('click', () => {
+    selectElement('.menu__btn').classList.toggle('open');
+});
+
+// const hamburger = document.querySelector('.menu__btn');
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('open');
+// });
