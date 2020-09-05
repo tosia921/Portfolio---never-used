e9218e72f6fb967b69c56a6490e0f6b76f827d11
Tomasz Posiadala <tomaszposiadala@gmail.com>
2020-09-06 00:12:53 +0100

    finished responsive navbar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import './styles/main.scss';
 
-//Function to select elements
+//Functions to select element/element's
 
 const selectElement = (element) => document.querySelector(element);
+const selectAllElements = (element) => document.querySelectorAll(element);
 
+//EventListener Function
 
 selectElement('.menu__btn').addEventListener('click', () => {
+    //animated hamburger button
     selectElement('.menu__btn').classList.toggle('open');
+    //showing menu list background on click
+    selectElement('.header__nav__list').classList.toggle('show');
+    //drop down of each menu items
+    selectAllElements('.header__nav__list__item--link').forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ``;
+        } else {
+            link.style.animation = `drop 0.5s ease forwards ${index / 3.5 + 0.2}s`;
+        }
+    });
 });
-
-// const hamburger = document.querySelector('.menu__btn');
-// hamburger.addEventListener('click', () => {
-//     hamburger.classList.toggle('open');
-// });
